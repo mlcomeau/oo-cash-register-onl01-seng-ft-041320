41415ef5ffa66cb4495740ab78965d55fa9f4d36
Megan Comeau <mlcomeau99@gmail.com>
2020-04-21 22:20:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+  attr_accessor :total, :employee_discount
 
+  def initialize( n = 0 )
+    @total = 0
+    @employee_discount = n
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+  end
+
+  def apply_discount
+    if @employee_discount == 0
+      puts "There is no discount to apply."
+    else
+
+
+
+end
